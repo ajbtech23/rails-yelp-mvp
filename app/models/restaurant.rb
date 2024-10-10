fe1a1bfa11b04e69d4b0bd63b79dab3ad4ff05c2
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-10 18:40:25 +0100

    merging the kitchens

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french Indian belgian],
+  validates :category, inclusion: { in: %w[chinese italian japanese french indian belgian turkish],
     message: "%{value} is not a valid size" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+restaurants = []
+
+restaurants << Restaurant.create(name: "Tava", address: "23 Kingsland Road", phone_number: "07510-561-203", category: "turkish")
+restaurants << Restaurant.create(name: "Dishoom", address: "187 Alecia Road", phone_number: "07455-962-732", category: "indian")
+restaurants << Restaurant.create(name: "Yi Ban", address: "Opposite moxy hotel", phone_number: "0775-221-388", category: "chinese")
+restaurants << Restaurant.create(name: "Le Hain", address: "37 Rue de Canelle", phone_number: "404020420402042", category: "french")
+restaurants << Restaurant.create(name: "Sticks and Sushi", address: "Tokyo Haneda Airport", phone_number: "24420420524", category: "japanese")
+
+counter = 0
+
+while counter < 5
+  Review.create(rating: counter, content: "adfadgdagadga", restaurant_id: restaurants[counter].id)
+  counter = counter + 1
+end
